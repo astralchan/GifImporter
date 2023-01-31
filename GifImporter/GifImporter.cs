19a894807488692b2fb52297da39ff45e889a07c
astralchan <astral@astralchan.xyz>
2023-01-31 00:54:59 -0700

    styling

--- GifImporter/GifImporter.cs
+++ GifImporter/GifImporter.cs
@@ -1,4 +1,6 @@
-﻿using HarmonyLib;
+﻿// vim: ts=4 sw=4 noet cc=160
+
+using HarmonyLib;
 using NeosModLoader;
 using FrooxEngine;
 using BaseX;
@@ -10,191 +12,192 @@ using System.Drawing;
 
 namespace GifImporter
 {
-    public class GifImporter : NeosMod
-    {
-        public override string Name => "GifImporter";
-        public override string Author => "amber";
-        public override string Version => "1.1.3";
-        public override string Link => "https://github.com/astralchan/GifImporter";
+	public class GifImporter : NeosMod
+	{
+		public override string Name => "GifImporter";
+		public override string Author => "amber";
+		public override string Version => "1.1.3";
+		public override string Link => "https://github.com/astralchan/GifImporter";
 
-        [AutoRegisterConfigKey]
-        public static ModConfigurationKey<bool> KEY_SQUARE = new ModConfigurationKey<bool>("square_images", "Generate square tiles (sometimes has bigger size)", () => false);
-        public static ModConfiguration config;
+		[AutoRegisterConfigKey]
+		public static ModConfigurationKey<bool> KEY_SQUARE = new ModConfigurationKey<bool>(
+			"square_images", "Generate square tiles (sometimes has bigger size)", () => false);
+		public static ModConfiguration config;
 
-        public override void OnEngineInit()
-        {
-            Harmony harmony = new Harmony("tk.kawaiiamber.gifimporter");
-            harmony.PatchAll();
-            config = GetConfiguration();
-        }
+		public override void OnEngineInit()
+		{
+			Harmony harmony = new Harmony("tk.kawaiiamber.gifimporter");
+			harmony.PatchAll();
+			config = GetConfiguration();
+		}
 
-        [HarmonyPatch(typeof(ImageImporter), "ImportImage")]
-        class FileImporterPatch
-        {
-            public static bool Prefix(string path, ref Task __result, Slot targetSlot, float3? forward, StereoLayout stereoLayout, ImageProjection projection, bool setupScreenshotMetadata, bool addCollider)
-            {
-                Uri uri = new Uri(path);
-                Image image = null;
-                bool validGif = false;
-                // Local file import vs URL import
+		[HarmonyPatch(typeof(ImageImporter), "ImportImage")]
+		class FileImporterPatch
+		{
+			public static bool Prefix(string path, ref Task __result, Slot targetSlot, float3? forward, StereoLayout stereoLayout, ImageProjection projection,
+				bool setupScreenshotMetadata, bool addCollider)
+			{
+				Uri uri = new Uri(path);
+				Image image = null;
+				bool validGif = false;
 
-                if (uri.Scheme == "file" && string.Equals(Path.GetExtension(path), ".gif", StringComparison.OrdinalIgnoreCase))
-                {
-                    image = Image.FromStream(File.OpenRead(path));
-                    validGif = true;
-                }
-                else if (uri.Scheme == "http" || uri.Scheme == "https")
-                {
-                    var client = new System.Net.WebClient();
-                    image = Image.FromStream(client.OpenRead(uri));
-                    var type = client.ResponseHeaders.Get("content-type");
-                    if(type == "image/gif")
-                    {
-                        validGif = true;
-                    }
-                }
-                /* TODO: Support neosdb links
+				// Local file import vs URL import
+				if (uri.Scheme == "file" && string.Equals(Path.GetExtension(path), ".gif", StringComparison.OrdinalIgnoreCase))
+				{
+					image = Image.FromStream(File.OpenRead(path));
+					validGif = true;
+				}
+				else if (uri.Scheme == "http" || uri.Scheme == "https")
+				{
+					var client = new System.Net.WebClient();
+					image = Image.FromStream(client.OpenRead(uri));
+					var type = client.ResponseHeaders.Get("content-type");
+					if (type == "image/gif") validGif = true;
+				}
+				/* TODO: Support neosdb links
 				else if (uri.Scheme == "neosdb"){
 					// neosdb handling here
 				} */
-                if (!validGif)
-                {
-                    Error(new ArgumentException($"Image is not a gif or the URI Scheme {uri.Scheme} is not supported"));
-                    image?.Dispose();
-                    return true;
-                }
-                __result = targetSlot.StartTask(async delegate ()
-                {
+				if (!validGif)
+				{
+					Error(new ArgumentException($"Image is not a gif or the URI Scheme {uri.Scheme} is not supported"));
+					image?.Dispose();
+					return true;
+				}
+				__result = targetSlot.StartTask(async delegate ()
+				{
 
-                    await default(ToBackground);
-                    // Load the image
-                    int frameCount = 0;
-                    float frameDelay = 0;
-                    var frameWidth = 0;
-                    var frameHeight = 0;
-                    int gifRows = 0;
-                    int gifCols = 0;
-                    const int PropertyTagFrameDelay = 0x5100; // https://docs.microsoft.com/en-us/dotnet/api/system.drawing.imaging.propertyitem.id PropertyTagFrameDelay
-                    Bitmap spriteSheet = null;
-                    string spritePath = Path.Combine(Engine.Current.AppPath, "nml_mods", "tmp_sheet.png");
+					await default(ToBackground);
+					// Load the image
+					int frameCount = 0;
+					float frameDelay = 0;
+					var frameWidth = 0;
+					var frameHeight = 0;
+					int gifRows = 0;
+					int gifCols = 0;
+					// https://docs.microsoft.com/en-us/dotnet/api/system.drawing.imaging.propertyitem.id PropertyTagFrameDelay
+					const int PropertyTagFrameDelay = 0x5100;
+					Bitmap spriteSheet = null;
+					string spritePath = Path.Combine(Engine.Current.AppPath, "nml_mods", "tmp_sheet.png");
 
-                    try
-                    {
-                        frameCount = image.GetFrameCount(FrameDimension.Time);
+					try
+					{
+						frameCount = image.GetFrameCount(FrameDimension.Time);
 
-                        FrameDimension frameDimension = new FrameDimension(image.FrameDimensionsList[0]);
-                        frameWidth = image.Width;
-                        frameHeight = image.Height;
+						FrameDimension frameDimension = new FrameDimension(image.FrameDimensionsList[0]);
+						frameWidth = image.Width;
+						frameHeight = image.Height;
 
-                        // Get the times stored in the image
-                        var times = image.GetPropertyItem(PropertyTagFrameDelay).Value;
+						// Get the times stored in the image
+						var times = image.GetPropertyItem(PropertyTagFrameDelay).Value;
 
-                        if (config.GetValue(KEY_SQUARE))
-                        {
-                            // Calculate amount of cols and rows
-                            float ratio = (float)frameWidth / frameHeight;
-                            var cols = MathX.Sqrt(frameCount / ratio);
-                            gifCols = MathX.RoundToInt(cols);
-                            gifRows = frameCount / gifCols + ((frameCount % gifCols != 0) ? 1 : 0);
-                        }
-                        else
-                        {
-                            gifCols = frameCount;
-                            gifRows = 1;
-                        }
+						if (config.GetValue(KEY_SQUARE))
+						{
+							// Calculate amount of cols and rows
+							float ratio = (float)frameWidth / frameHeight;
+							var cols = MathX.Sqrt(frameCount / ratio);
+							gifCols = MathX.RoundToInt(cols);
+							gifRows = frameCount / gifCols + ((frameCount % gifCols != 0) ? 1 : 0);
+						}
+						else
+						{
+							gifCols = frameCount;
+							gifRows = 1;
+						}
 
-                        // Create a new image
-                        spriteSheet = new Bitmap(frameWidth * gifCols, frameHeight * gifRows);                        
-                        int delay = 0;
-                        using (Graphics g = Graphics.FromImage(spriteSheet))
-                        {
-                            for (int i = 0; i < gifRows; i++)
-                            {
-                                for(int j = 0; j < gifCols; j++)
-                                {
-                                    if(i * gifCols + j >= frameCount)
-                                    {
-                                        break;
-                                    }
-                                    //convert 4 bit value to integer
-                                    var duration = BitConverter.ToInt32(times, 4 * ((i * gifCols) + j));
-                                    //Set the write frame before we save it
-                                    image.SelectActiveFrame(FrameDimension.Time, i * gifCols + j);
-                                    g.DrawImage(image, frameWidth * j, frameHeight * i);
-                                    delay += duration;
-                                }
-                            }
-                            frameDelay = 100 / (delay / frameCount);
-                        }
+						// Create a new image
+						spriteSheet = new Bitmap(frameWidth * gifCols, frameHeight * gifRows);                        
+						int delay = 0;
+						using (Graphics g = Graphics.FromImage(spriteSheet))
+						{
+							for (int i = 0; i < gifRows; i++)
+							{
+								for(int j = 0; j < gifCols; j++)
+								{
+									if(i * gifCols + j >= frameCount)
+									{
+										break;
+									}
+									//convert 4 bit value to integer
+									var duration = BitConverter.ToInt32(times, 4 * ((i * gifCols) + j));
+									//Set the write frame before we save it
+									image.SelectActiveFrame(FrameDimension.Time, i * gifCols + j);
+									g.DrawImage(image, frameWidth * j, frameHeight * i);
+									delay += duration;
+								}
+							}
+							frameDelay = 100 / (delay / frameCount);
+						}
 
-                        // Save the image
-                        spriteSheet.Save(spritePath);
-                    }
-                    finally
-                    {
-                        image.Dispose();
-                    }
+						// Save the image
+						spriteSheet.Save(spritePath);
+					}
+					finally
+					{
+						image.Dispose();
+					}
 
-                    Debug($"Image saved as {spritePath}");
+					Debug($"Image saved as {spritePath}");
 
-                    LocalDB localDB = targetSlot.World.Engine.LocalDB;
-                    Uri localUri = await localDB.ImportLocalAssetAsync(spritePath, LocalDB.ImportLocation.Copy).ConfigureAwait(continueOnCapturedContext: false);
+					LocalDB localDB = targetSlot.World.Engine.LocalDB;
+					Uri localUri = await
+						localDB.ImportLocalAssetAsync(spritePath, LocalDB.ImportLocation.Copy).ConfigureAwait(continueOnCapturedContext: false);
 
-                    File.Delete(spritePath);
+					File.Delete(spritePath);
 
-                    await default(ToWorld);
+					await default(ToWorld);
 
-                    targetSlot.Name = Path.GetFileNameWithoutExtension(spritePath);
-                    if (forward.HasValue)
-                    {
-                        float3 from = forward.Value;
-                        float3 to = float3.Forward;
-                        targetSlot.LocalRotation = floatQ.FromToRotation(in from, in to);
-                    }
-                    StaticTexture2D tex = targetSlot.AttachComponent<StaticTexture2D>();
-                    tex.URL.Value = localUri;
-                    ImageImporter.SetupTextureProxyComponents(targetSlot, tex, stereoLayout, projection, setupScreenshotMetadata);
-                    if (projection != 0)
-                    {
-                        ImageImporter.Create360Sphere(targetSlot, tex, stereoLayout, projection, addCollider);
-                    }
-                    else
-                    {
-                        while (!tex.IsAssetAvailable)
-                        {
-                            await default(NextUpdate);
-                        }
-                        ImageImporter.CreateQuad(targetSlot, tex, stereoLayout, addCollider);
-                    }
-                    if (setupScreenshotMetadata)
-                    {
-                        targetSlot.GetComponentInChildren<PhotoMetadata>()?.NotifyOfScreenshot();
-                    }
+					targetSlot.Name = Path.GetFileNameWithoutExtension(spritePath);
+					if (forward.HasValue)
+					{
+						float3 from = forward.Value;
+						float3 to = float3.Forward;
+						targetSlot.LocalRotation = floatQ.FromToRotation(in from, in to);
+					}
+					StaticTexture2D tex = targetSlot.AttachComponent<StaticTexture2D>();
+					tex.URL.Value = localUri;
+					ImageImporter.SetupTextureProxyComponents(targetSlot, tex, stereoLayout, projection, setupScreenshotMetadata);
+					if (projection != 0)
+					{
+						ImageImporter.Create360Sphere(targetSlot, tex, stereoLayout, projection, addCollider);
+					}
+					else
+					{
+						while (!tex.IsAssetAvailable)
+						{
+							await default(NextUpdate);
+						}
+						ImageImporter.CreateQuad(targetSlot, tex, stereoLayout, addCollider);
+					}
+					if (setupScreenshotMetadata)
+					{
+						targetSlot.GetComponentInChildren<PhotoMetadata>()?.NotifyOfScreenshot();
+					}
 
-                    AtlasInfo _AtlasInfo = targetSlot.AttachComponent<AtlasInfo>();
-                    UVAtlasAnimator _UVAtlasAnimator = targetSlot.AttachComponent<UVAtlasAnimator>();
-                    TimeIntDriver _TimeIntDriver = targetSlot.AttachComponent<TimeIntDriver>();
-                    _AtlasInfo.GridFrames.Value = frameCount;
-                    _AtlasInfo.GridSize.Value = new int2(gifCols, gifRows);
-                    _TimeIntDriver.Scale.Value = frameDelay;
-                    _TimeIntDriver.Repeat.Value = _AtlasInfo.GridFrames.Value;
-                    _TimeIntDriver.Target.Target = _UVAtlasAnimator.Frame;
-                    _UVAtlasAnimator.AtlasInfo.Target = _AtlasInfo;
+					AtlasInfo _AtlasInfo = targetSlot.AttachComponent<AtlasInfo>();
+					UVAtlasAnimator _UVAtlasAnimator = targetSlot.AttachComponent<UVAtlasAnimator>();
+					TimeIntDriver _TimeIntDriver = targetSlot.AttachComponent<TimeIntDriver>();
+					_AtlasInfo.GridFrames.Value = frameCount;
+					_AtlasInfo.GridSize.Value = new int2(gifCols, gifRows);
+					_TimeIntDriver.Scale.Value = frameDelay;
+					_TimeIntDriver.Repeat.Value = _AtlasInfo.GridFrames.Value;
+					_TimeIntDriver.Target.Target = _UVAtlasAnimator.Frame;
+					_UVAtlasAnimator.AtlasInfo.Target = _AtlasInfo;
 
-                    QuadMesh _QuadMesh = targetSlot.GetComponent<QuadMesh>();
-                    _QuadMesh.Size.Value = new float2(frameWidth, frameHeight).Normalized;
+					QuadMesh _QuadMesh = targetSlot.GetComponent<QuadMesh>();
+					_QuadMesh.Size.Value = new float2(frameWidth, frameHeight).Normalized;
 
-                    UnlitMaterial _UnlitMaterial = targetSlot.GetComponent<UnlitMaterial>();
-                    _UVAtlasAnimator.ScaleField.Target = _UnlitMaterial.TextureScale;
-                    _UVAtlasAnimator.OffsetField.Target = _UnlitMaterial.TextureOffset;
+					UnlitMaterial _UnlitMaterial = targetSlot.GetComponent<UnlitMaterial>();
+					_UVAtlasAnimator.ScaleField.Target = _UnlitMaterial.TextureScale;
+					_UVAtlasAnimator.OffsetField.Target = _UnlitMaterial.TextureOffset;
 
-                    // Set inventory preview to first frame
-                    ItemTextureThumbnailSource _inventoryPreview = targetSlot.GetComponent<ItemTextureThumbnailSource>();
-                    _inventoryPreview.Crop.Value = new Rect(0, 0, 1f / (float)gifCols, 1f / (float)gifRows);
-                });
+					// Set inventory preview to first frame
+					ItemTextureThumbnailSource _inventoryPreview = targetSlot.GetComponent<ItemTextureThumbnailSource>();
+					_inventoryPreview.Crop.Value = new Rect(0, 0, 1f / (float)gifCols, 1f / (float)gifRows);
+				});
 
-                return false;
-            }
-        }
-    }
+				return false;
+			}
+		}
+	}
 }
