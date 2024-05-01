790f7f49e3cad454f11b3209ece1beb4fc732a35
Sinduy <sjsanjsrh@naver.com>
2024-05-01 23:51:08 +0900

    Patch component insertion
    update to version 1.1.9
    fix wiring issues
    update component insertion

--- GifImporter.cs
+++ GifImporter.cs
@@ -18,7 +18,7 @@ public class GifImporter : ResoniteMod
     {
     public override string Name    => "GifImporter";
     public override string Author  => "astral";
-    public override string Version => "1.1.8";
+    public override string Version => "1.1.9";
     public override string Link    => "https://github.com/astralchan/GifImporter";
 
     [AutoRegisterConfigKey]
@@ -205,12 +205,13 @@ public class GifImporter : ResoniteMod
                 _TimeIntDriver.Target.Target = _UVAtlasAnimator.Frame;
                 _UVAtlasAnimator.AtlasInfo.Target = _AtlasInfo;
 
-                QuadMesh _QuadMesh = targetSlot.GetComponent<QuadMesh>();
-                _QuadMesh.Size.Value = new float2(frameWidth, frameHeight).Normalized;
+                TextureSizeDriver _TextureSizeDriver = targetSlot.GetComponent<TextureSizeDriver>();
+                _TextureSizeDriver.Premultiply.Value = new float2(gifRows, gifCols);
 
                 UnlitMaterial _UnlitMaterial = targetSlot.GetComponent<UnlitMaterial>();
                 _UVAtlasAnimator.ScaleField.Target = _UnlitMaterial.TextureScale;
                 _UVAtlasAnimator.OffsetField.Target = _UnlitMaterial.TextureOffset;
+                _UnlitMaterial.BlendMode.Value = BlendMode.Cutout;
 
                 // Set inventory preview to first frame
                 ItemTextureThumbnailSource _inventoryPreview = targetSlot.GetComponent<ItemTextureThumbnailSource>();
