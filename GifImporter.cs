5374fb53deb979a3d4af5535ed4f4be7673c7bfb
astral <astral@astralchan.xyz>
2024-07-24 21:52:58 -0700

    update

--- GifImporter.cs
+++ GifImporter.cs
@@ -18,7 +18,7 @@ public class GifImporter : ResoniteMod
     {
     public override string Name    => "GifImporter";
     public override string Author  => "astral";
-    public override string Version => "1.1.9";
+    public override string Version => "1.2.0";
     public override string Link    => "https://github.com/astralchan/GifImporter";
 
     [AutoRegisterConfigKey]
